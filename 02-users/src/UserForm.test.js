ea1fff8938476e2d42824b8a1ae95ae676f006bd
Noah Kim <dongwoo_333@hanmail.net>
2023-02-13 07:25:24 +1100

    15. Recording Function Calls

--- 02-users/src/UserForm.test.js
+++ 02-users/src/UserForm.test.js
@@ -18,8 +18,12 @@ test('it shows two inputs and a button', () => {
 
 test('it calls onUserAdd when the form is submitted', () => {
   // NOT THE BEST IMPLEMENTATION
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
   // Try to render my component
-  render(<UserForm />);
+  render(<UserForm onUserAdd={callback} />);
 
   // Find the two inputs
   const [nameInput, emailInput] = screen.getAllByRole('textbox');
@@ -33,6 +37,15 @@ test('it calls onUserAdd when the form is submitted', () => {
   user.keyboard('jane@jane.com');
 
   // Find the button
+  const button = screen.getByRole('button');
+
   // Simulate clicking the button
+  user.click(button);
+
   // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({
+    name: 'jane',
+    email: 'jane@jane.com',
+  });
 });
