0110c6fa2a9df92532d5284abebd0630c3bb145e
Noah Kim <dongwoo_333@hanmail.net>
2023-02-13 07:19:12 +1100

    14. Simulating User Events

--- 02-users/src/UserForm.test.js
+++ 02-users/src/UserForm.test.js
@@ -15,3 +15,24 @@ test('it shows two inputs and a button', () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test('it calls onUserAdd when the form is submitted', () => {
+  // NOT THE BEST IMPLEMENTATION
+  // Try to render my component
+  render(<UserForm />);
+
+  // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  // Simulate typing in a name
+  user.click(nameInput);
+  user.keyboard('jane');
+
+  // Simulate typing in an email
+  user.click(emailInput);
+  user.keyboard('jane@jane.com');
+
+  // Find the button
+  // Simulate clicking the button
+  // Assertion to make sure 'onUserAdd' gets called with email/name
+});
