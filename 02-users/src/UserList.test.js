4da57ce5ca600f8faed44dd2567bc72485440105
Noah Kim <dongwoo_333@hanmail.net>
2023-03-19 09:01:43 +1100

    22. Testing Table Contents

--- 02-users/src/UserList.test.js
+++ 02-users/src/UserList.test.js
@@ -26,4 +26,21 @@ test('render one row per user', () => {
   expect(rows).toHaveLength(2);
 });
 
-test('render the email and name of each user', () => {});
+test('render the email and name of each user', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+
+  render(<UserList users={users} />);
+
+  // screen.logTestingPlaygroundURL();
+
+  for (let user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
