81e7edbcea18bf31d373a18e8d67aa7d48cb3285
Noah Kim <dongwoo_333@hanmail.net>
2023-03-22 07:07:06 +1100

    40. When to use Async Queries

---
+++ 04-query-functions/LoadableColorList.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function fakeFetchColors() {
+  return Promise.resolve(['red', 'green', 'blue']);
+}
+
+function LoadableColorList() {
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    fakeFetchColors().then((c) => setColors(c));
+  }, []);
+
+  const renderedColors = colors.map((color) => {
+    return <li key={color}>{color}</li>;
+  });
+
+  return <ul>{renderedColors}</ul>;
+}
+
+export default LoadableColorList;
