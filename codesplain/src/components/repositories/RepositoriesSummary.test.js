3a616ce200879c2c1cc4d84acd43bc6677c8fb5c
Noah Kim <dongwoo_333@hanmail.net>
2023-03-28 16:37:32 +1100

    56. Flexible Queries

--- codesplain/src/components/repositories/RepositoriesSummary.test.js
+++ codesplain/src/components/repositories/RepositoriesSummary.test.js
@@ -12,7 +12,7 @@ test('displays information about the repository', () => {
 
   for (let key in repository) {
     const value = repository[key];
-    const element = screen.getByText(value);
+    const element = screen.getByText(new RegExp(value));
 
     expect(element).toBeInTheDocument();
   }
