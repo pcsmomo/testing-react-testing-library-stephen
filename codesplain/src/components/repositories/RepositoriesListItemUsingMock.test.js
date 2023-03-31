d64718ad52017f66444cae3aaa4b5912af30895e
Noah Kim <dongwoo_333@hanmail.net>
2023-03-31 13:42:02 +1100

    64. Module Mocks

---
+++ codesplain/src/components/repositories/RepositoriesListItemUsingMock.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+
+import RepositoriesListItem from './RepositoriesListItem';
+
+jest.mock('../tree/FileIcon', () => {
+  // Content of FileIcon.js
+  return () => {
+    return 'File Icon Component';
+  };
+});
+
+function renderComponent() {
+  const repository = {
+    full_name: 'facebook/react',
+    language: 'Javascript',
+    description: 'A js library',
+    owner: 'facebook',
+    name: 'react',
+    html_url: 'https://github.com/facebook/react',
+  };
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+}
+
+test('shows a link to the github homepage for this repository', async () => {
+  renderComponent();
+
+  // eslint-disable-next-line testing-library/no-debugging-utils
+  screen.debug();
+
+  // await screen.findByRole('img', { name: 'Javascript' });
+});
