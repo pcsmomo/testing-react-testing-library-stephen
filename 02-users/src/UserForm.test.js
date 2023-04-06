60d9188fd56a3d2c13a0697d726c462163615e54
Noah Kim <dongwoo_333@hanmail.net>
2023-04-07 09:38:27 +1000

    15. test was not wrapped in act(...) Warning and Test Failure

--- 02-users/src/UserForm.test.js
+++ 02-users/src/UserForm.test.js
@@ -16,7 +16,7 @@ test('it shows two inputs and a button', () => {
   expect(button).toBeInTheDocument();
 });
 
-test('it calls onUserAdd when the form is submitted', () => {
+test('it calls onUserAdd when the form is submitted', async () => {
   // NOT THE BEST IMPLEMENTATION
   const mock = jest.fn();
   // Try to render my component
@@ -31,37 +31,37 @@ test('it calls onUserAdd when the form is submitted', () => {
   });
 
   // Simulate typing in a name
-  user.click(nameInput);
-  user.keyboard('jane');
+  await user.click(nameInput);
+  await user.keyboard('jane');
 
   // Simulate typing in an email
-  user.click(emailInput);
-  user.keyboard('jane@jane.com');
+  await user.click(emailInput);
+  await user.keyboard('jane@jane.com');
 
   // Find the button
   const button = screen.getByRole('button');
 
   // Simulate clicking the button
-  user.click(button);
+  await user.click(button);
 
   // Assertion to make sure 'onUserAdd' gets called with email/name
   expect(mock).toHaveBeenCalled();
   expect(mock).toHaveBeenCalledWith({ name: 'jane', email: 'jane@jane.com' });
 });
 
-test('empties the two inputs after the form is submitted', () => {
+test('empties the two inputs after the form is submitted', async () => {
   render(<UserForm onUserAdd={() => {}} />);
 
   const nameInput = screen.getByRole('textbox', { name: /name/i });
   const emailInput = screen.getByRole('textbox', { name: /email/i });
   const button = screen.getByRole('button');
 
-  user.click(nameInput);
-  user.keyboard('jane');
-  user.click(emailInput);
-  user.keyboard('jane@jane.com');
+  await user.click(nameInput);
+  await user.keyboard('jane');
+  await user.click(emailInput);
+  await user.keyboard('jane@jane.com');
 
-  user.click(button);
+  await user.click(button);
 
   expect(nameInput).toHaveValue('');
   expect(emailInput).toHaveValue('');
